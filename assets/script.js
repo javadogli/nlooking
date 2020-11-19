3f3011beb4c3bf56dbffc9b46098411b6fa922e7
Dreta <yangmain3@gmail.com>
2020-11-19 17:57:49 +0800

    Add a volume toggle

--- assets/script.js
+++ assets/script.js
@@ -16,42 +16,32 @@
 
 // Please don't look at this. This is ugly AF.
 
-const display1 = document.getElementById("display-1");
 const display2 = document.getElementById("display-2");
 const text = document.getElementById("text");
 const otherText = document.getElementById("other-text");
 const descText = document.getElementById("description-text");
+const playbackToggle = document.getElementById("playback-toggle");
+const toggleOn = document.getElementById("toggle-on");
+const toggleOff = document.getElementById("toggle-off");
 const audio = document.getElementById("audio");
 
-let initialized = false;
-
 function initialize() {
-    display1.style.animationName = "fade-out";
-    display1.style.animationDuration = "200ms";
-    display1.style.animationTimingFunction = "ease-in";
-    display1.style.animationPlayState = "running";
+    display2.style.display = "flex";
+    display2.style.animationName = "fade-in";
+    display2.style.animationDuration = "200ms";
+    display2.style.animationTimingFunction = "ease-out";
+    display2.style.animationPlayState = "running";
+    display2.style.opacity = "1";
+    text.style.animationPlayState = "running";
 
     setTimeout(() => {
-        // This will be called after the initial fade out finishes.
-        display1.remove();
-        display2.style.display = "flex";
-        display2.style.animationName = "fade-in";
-        display2.style.animationDuration = "200ms";
-        display2.style.animationTimingFunction = "ease-out";
-        display2.style.animationPlayState = "running";
-        display2.style.opacity = "1";
-        text.style.animationPlayState = "running";
-        audio.play();
-
-        setTimeout(() => {
-            // This will be called after the title animation finishes.
-            otherText.style.opacity = "1";
-            otherText.style.animationName = "fade-in";
-            otherText.style.animationDuration = "500ms";
-            otherText.style.animationTimingFunction = "ease-out";
-            otherText.style.animationPlayState = "running";
-        }, 1700);
-    }, 200);
+        // This will be called after the title animation finishes.
+        otherText.style.opacity = "1";
+        otherText.style.animationName = "fade-in";
+        otherText.style.animationDuration = "500ms";
+        otherText.style.animationTimingFunction = "ease-out";
+        otherText.style.animationPlayState = "running";
+    }, 1700);
 
     let blur = 20;
     const interval = setInterval(() => {
@@ -63,14 +53,9 @@ function initialize() {
         // F Safari
         document.querySelector("body").setAttribute("style", "backdrop-filter: blur(" + blur + "px); -webkit-backdrop-filter: blur(" + blur + "px)");
     }, 10);
-    initialized = true;
 }
 
 function handleClick() {
-    if (!initialized) {
-        initialize();
-        return;
-    }
     if (clicking) {
         return;
     }
@@ -94,12 +79,24 @@ function handleClick() {
 }
 
 window.onkeyup = e => {
-    if (!initialized && e.key === " ") {
-        initialize();
-    } else if (initialized && (e.key === "Enter" || e.key === " ")) {
+    if (e.key === "Enter" || e.key === " ") {
         handleClick();
     }
 };
+
+playbackToggle.onclick = () => {
+    if (audio.paused) {
+        audio.play();
+        toggleOff.style.display = "none";
+        toggleOn.style.display = "block";
+    } else {
+        audio.pause();
+        toggleOn.style.display = "none";
+        toggleOff.style.display = "block";
+    }
+};
+
+initialize();
 
 const messages = [
     "Stay-at-home Developer",
@@ -111,11 +108,11 @@ const messages = [
     "Doing:",
     "Working on <a href=\"https://github.com/Spock-App\" title=\"Spock\">Spock</a>",
     "Learning <a href=\"https://www.coursera.org/learn/nand2tetris2\">nand2tetris Part 2</a>",
-    "Please add me on Discord if interested"
+    "Preferred contact method is Discord."
 ];
 
 let current = 0;
 let clicking = false;
 
-window.onclick = handleClick;
-window.ontouchend = handleClick;
+display2.onclick = handleClick;
+display2.ontouchend = handleClick;
